03dba046cda214dc7811c82bd00393503ddf8d01
nidaparveen <nidavattathuru@gmail.com>
2026-08-13 20:32:36 +0530

    day 2: thinking

---
+++ day2.py
@@ -0,0 +1,9 @@
+num = {}
+
+for i in num:
+    expense=input("enter money")
+    category=input("enter category")        
+    date=input("enter date")
+    num.append(expense,category,date)
+
+print(num)
