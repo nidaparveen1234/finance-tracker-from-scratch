a6fdcfa1b08251615bcffc5f6b987eb28555cfcd
nidaparveen <nidavattathuru@gmail.com>
2026-08-13 22:16:23 +0530

    day 2: thinking

--- day2.py
+++ day2.py
@@ -1,9 +1,25 @@
 num = {}
 
-for i in num:
+for key in range(1):
+
     expense=input("enter money")
     category=input("enter category")        
     date=input("enter date")
-    num.append(expense,category,date)
+
+    # num[expense] = category 
+    # num[expense] = date 
+    # here i am over writing my value turns out i am doing it 
+    # wrong
+
+    num[expense] = {
+        "category" : category,
+        "date" : date 
+    }
+    # this helps create dict inside dict 
+    # it is helpfull so remember
+# enter money12
+# enter categoryfood
+# enter date13/8
+# {'12': {'category': 'food', 'date': '13/8'}}
 
 print(num)
