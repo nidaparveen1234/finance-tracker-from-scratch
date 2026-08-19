4c307eade8a2b68fb9dbfee523fafec06a4f7189
nidaparveen1234 <nidavattathuru@gmail.com>
2026-08-19 22:05:45 +0530

    Add plan for item categorization and total calculation
    Outline the plan to categorize items and calculate totals.

---
+++ day06.py
@@ -0,0 +1,5 @@
+to add category of each items and total them :
+1, we need to change it into catgory so each item first needs to be
+categorized sooo i am thinking soo each catgory needs a seprete 
+dict with category,prize and we need to create it
+2, after creating a dict we could find the category and calculate them,
